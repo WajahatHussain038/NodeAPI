4eb0c661a0f86612123374a7e61a9cd61e16e5e5
Wajahat Hussain (IS) <Wajahat.Hussain@styletextile.com>
2026-08-12 14:44:51 +0500

    Jwt Verification Athorization Implimenatio in Progress

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,23 +1,31 @@
 const jwt = require("jsonwebtoken");
 const config = require("config");
 function auth(req, res, next) {
-  console.log("Auth Method");
-  if (!req.header("x-auth-token"))
-    return res.status(401).send("No Token Provided");
+  const authHeader = req.headers["authorization"];
+  // Format typically looks like: "Bearer <TOKEN>"
+  const token = authHeader && authHeader.split(" ")[1];
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Access Denied: No Token Provided" });
+  }
 
   try {
-    const tokenStatus = jwt.verify(
-      req.header("x-auth-token"),
-      config.get("jwtPrivateKey"),
-    );
-    // if (!tokenStatus) {
-    console.log("Going to Post Method");
-    req.user = tokenStatus;
+    // 2. Verify the token using your secret key
+    const verified = jwt.verify(token, config.get("jwtPrivateKey"));
+
+    // 3. Attach the decoded payload (e.g., user id) to the request object
+    // req.user = verified;
+    console.log(verified);
+    res.status(200).json({ message: verified });
+    // 4. Move to the next middleware or route handler
     next();
-    // }
-  } catch {
-    return res.status(401).send("Invalid Token Provided");
+  } catch (error) {
+    res.status(403).json({ message: "Invalid or Expired Token" });
   }
+
+  // console.log(token);
+  // return res.send("Token Validation Method");
 }
 
 module.exports = auth;
